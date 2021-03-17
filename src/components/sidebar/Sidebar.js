819f0201a5948feadf5b9a7cfaf60595184b458c
kito0 <drewshah0@gmail.com>
2021-03-16 21:30:19 -0600

    updated sidebar css

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './css/sidebar.css';
 import SidebarOption from './SidebarOption';
 import { IconButton } from '@material-ui/core';
-import { EventAvailable } from '@material-ui/icons';
+import {
+	AccountBalanceWallet,
+	EventAvailable,
+	Notifications,
+} from '@material-ui/icons';
 
 export default function Sidebar() {
 	return (
 		<div className="sidebar">
 			<div className="sidebar_header">
+				<img src={process.env.PUBLIC_URL + '/logo.png'} className="image" />
 				<h2 className="title">Plannr</h2>
 			</div>
 			<SidebarOption active Icon={EventAvailable} text="Deadlines" />
+			<SidebarOption Icon={Notifications} text="Reminders" />
+			<SidebarOption Icon={AccountBalanceWallet} text="Budget" />
 		</div>
 	);
 }
